d69ce737da6f4b257339704e7137be390a291619
AhmadIbrahimFCAI <ahmad2005717@gmail.com>
2026-09-12 03:41:48 -0500

    create  the LLMInterface and its child

---
+++ src/stores/llm/LLMEnums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class LLMEnums(Enum):
+
+    OPENAI = "OPENAI"
+    COHERE = "COHERE"
+
+
+class OpenAIEnums(Enum):
+    SYSYEM = "system"
+    USER = "user"
+    ASSISTANT = "assistant"
+
